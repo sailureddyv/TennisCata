1ea119a6b25d5d683393a1f1bb53046ad636636a
unknown <SVUTUKUR@WGC100CYMDR72.fmcc.ford.com>
2017-06-06 09:03:47 -0400

    add tennis cata project

---
+++ src/test/java/com/demo/TennisCata/TennisCataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.TennisCata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TennisCataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
